a21bbccb8f1e42d7a703b8e868b6be6d977b06f3
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-27 13:08:15 -0700

    build the increasing and decreasing parts of the triangle

--- script1.js
+++ script1.js
@@ -11,3 +11,14 @@
 // Triangle pattern
 let pattern = "";
 const maxRows = 4;
+
+// increasing part of the triangle
+for (let i = 1; i <= maxRows; i++) {
+    pattern += "#".repeat(i) + "\n";
+  }
+
+  
+// decreasing part of the triangle
+for (let i = maxRows - 1; i >= 1; i--) {
+    pattern += "#".repeat(i) + "\n";
+  }
