16e328e40b37bd3b114dac48e5a8200d1b0c7418
gonzalez-7 <20agonzalezp@gmail.com>
2024-10-27 13:15:50 -0700

    Implement Fizz Condition Divisible by 3

--- script2.js
+++ script2.js
@@ -12,6 +12,9 @@
 
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);  // Temporary: print each number
+    if (i % 3 === 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
   }
-  
